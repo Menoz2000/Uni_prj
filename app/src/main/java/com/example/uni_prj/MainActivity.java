c77b9e1c35f95b127a4b011fc3669927a43cd2f8
Andrea Soncini <277224@studenti.unimore.it>
2021-07-02 12:11:58 +0200

    barra di navigazione con ?eccezioni?

--- app/src/main/java/com/example/uni_prj/MainActivity.java
+++ app/src/main/java/com/example/uni_prj/MainActivity.java
@@ -4,20 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final BottomNavigationView navigationView = findViewById(R.id.bottomNavigationView);
+        try {
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+            NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-        NavigationView Nav=findViewById(R.id.my_nav)
+        }catch (RuntimeException e) {
+
+
+        }
 
     }
 }
